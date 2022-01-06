1bc65b2d0cd02667ad419b28763a6f9629aa4daa
David Charif <davidcharif@hotmail.com>
2022-01-05 21:58:24 -0500

    Validacion numeros negativos o cero

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-
+// Extracion del DOM
 let currencyInput = document.getElementById("currencyInput");
 let currencyOutput = document.getElementById("currencyOutput");
+let container = document.getElementById("result");
+// Arrays info
 let moneda = ['Elige tu Moneda','Dolar','Peso Mexicano','Peso Colombiano','Euro','Libra Esterlina'];
 let dic =  {"Dolar":1,"Peso Mexicano":0.049,"Peso Colombiano":0.00025,"Euro":1.13, "Libra Esterlina":1.36}
-let container = document.getElementById("result");
+// Fragmentos y contenido Dinamico
 let alert = document.createElement("p")
 let fragmentInput = new DocumentFragment();
 let fragmentOutput = new DocumentFragment();
-
+// Se recorre el primer array y se adjuntan los elementos DOM
 moneda.forEach((text) => {
     let option = document.createElement("OPTION")
     option.innerHTML = text;
@@ -20,20 +22,17 @@ moneda.forEach((text) => {
     option.setAttribute("value",text);
     fragmentOutput.appendChild(option)
     })
-
+// Se agregan al DOM
 currencyInput.appendChild(fragmentInput) 
 currencyOutput.appendChild(fragmentOutput)
-/*
-25000 pesos colombianos a pesos mexicanos = 
 
-*/
-
+// Con la funcion se tomara el dolar como referencia de las monedas para su cambio
 let convertirFunc = () => {
     let inputBox = document.getElementById("input1").value
     let numberInput = parseInt(inputBox,10);
     let dolarizedInput;
     let inputConverted;
-    if(isNaN(numberInput)){
+    if(isNaN(numberInput) || numberInput < 0){
         
         alert.textContent = "Error de validacion";
         container.appendChild(alert)
